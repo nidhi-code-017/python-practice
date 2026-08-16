059cd22be6969beaa48ecd3b56456c4044c72cb4
sreenidhi <sreenidhiyadla@gmail.com>
2026-08-16 12:22:20 +0530

    error handling

--- foundations/error_handling.py
+++ foundations/error_handling.py
@@ -25,3 +25,26 @@ while True:
      operation = input("Enter the operation (+, -, *, /): ") 
      result = calculate(num1, num2, operation)
      print("The result is:", result)
+
+#Write a program that takes a list of numbers from the user (keep asking until they type "done"), then prints:
+# The unique numbers (use a set)
+# How many duplicates were removed
+# The numbers as a tuple (sorted)
+
+list_of_numbers = []
+while True:
+     num = input("Enter numbers : ")
+     if num == "done":
+        break
+     try:
+          num = int(num)
+          list_of_numbers.append(num)
+     except ValueError:
+          print("That's not a valid number.")
+          continue
+unique_numbers = set(list_of_numbers)
+duplicate_numbers = len(list_of_numbers) - len(unique_numbers)
+sorted_tuple = tuple(sorted(unique_numbers))
+print("Unique numbers:", unique_numbers)
+print("Duplicates removed:", duplicate_numbers)
+print("Sorted tuple:", sorted_tuple)
